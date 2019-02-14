7d1c814aeaf30f06a9df01780bd606ca99b9915e
BohdanTarchanin <btarchanin@gmail.com>
2019-02-14 17:39:34 +0200

    new controller/model songs and change root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  root 'songs#index'
 
+
+  devise_for :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190214113402) do
+ActiveRecord::Schema.define(version: 20190214153804) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.string "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
