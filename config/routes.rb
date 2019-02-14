44c06edbe7e788976ba82fbedcb2dc531c92d6f5
BohdanTarchanin <btarchanin@gmail.com>
2019-02-14 18:05:23 +0200

    update controller songs

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'songs#index'
-
-
+  resources :songs
   devise_for :users
 end
