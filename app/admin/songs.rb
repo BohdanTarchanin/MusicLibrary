2b9644584c38f9912b67ec3a2e7575a8c61a656c
BohdanTarchanin <btarchanin@gmail.com>
2019-02-14 17:59:07 +0200

    update permits for activeadmin

--- app/admin/songs.rb
+++ app/admin/songs.rb
@@ -1,15 +1,15 @@
 ActiveAdmin.register Song do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
 permit_params :name,:text,:desc, songs: []
+
+form(html: { multipart: true }) do |f|
+    f.inputs do
+      f.input :name
+      f.input :text
+      f.input :desc
+      f.file_field :songs, multiple: true
+    end
+   f.actions
+end
+
 end
